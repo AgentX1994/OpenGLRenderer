ddb75c7079147bb7c080851a849900b983b44f80
John Asper <agentx1994@gmail.com>
2016-03-24 20:59:23 -0400

    Initial Commit

---
+++ OpenGLThing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLThing
+//
+//  Created by John Asper on 2016/3/84.
+//  Copyright © 2016 John Asper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
